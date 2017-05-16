073fa3b1da23cd812c1afa836e99433b6a1f7319
Simon F <github@19r.de>
2017-05-16 14:45:52 +0200

    v0.0.1
    Grundgerüst

---
+++ WinData-CleanUp/start.ps1
@@ -0,0 +1,28 @@
+﻿    ###    Simon Fieber IT-Services    ###
+    ###     Coded by: Simon Fieber     ###
+    ###     Visit:  simonfieber.it     ###
+
+cls
+
+### Einstellungen ###
+
+    # Welcher Ordner soll überwacht werden?
+    $folder    = ""
+
+    # Wie soll der temporäre Papierkorb heißen?
+    $junkname  = ""
+
+    # Wie alt dürfen die Daten im oben angegebenen Ordner werden, bevor sie verschoben werden?
+    $moveage   = ""
+
+    # Wie alt dürfen die Daten im temporären Papierkorb werden, bevor sie endgültig gelöscht werden?
+    $deleteage = ""
+
+### Startbildschirm ###
+function startbildschirm {
+        Write-Host "╔═══════════════════════════════════════════════════════════════════════════════╗"
+        Write-Host "║ Windows Data Clean Up                                                         ║"
+        Write-Host "║                                                                        v0.0.1 ║"
+        Write-Host "╚═══════════════════════════════════════════════════════════════════════════════╝"
+}
+
