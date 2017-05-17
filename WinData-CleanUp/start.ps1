e10197d64107fe51455ccb3204291c48c1a8f905
Simon F <github@19r.de>
2017-05-17 11:23:04 +0200

    v0.1.1
    Funktion: Report erstellen

--- WinData-CleanUp/start.ps1
+++ WinData-CleanUp/start.ps1
@@ -17,13 +17,38 @@ cls
 
     # Wie alt dürfen die Daten im temporären Papierkorb werden, bevor sie endgültig gelöscht werden?
     $deleteage = ""
+    # Möchten Sie einen Report nach jedem verschieben?
+    # 0 = aus; 1 = ein
+    $movereport = 0
+
+    # Möchten Sie einen Report nach jedem löschvorgang?
+    # 0 = aus; 1 = ein
+    $deletereport = 0
 
 ### Startbildschirm ###
 function Startbildschirm {
         Write-Host "╔═══════════════════════════════════════════════════════════════════════════════╗"
         Write-Host "║ Windows Data Clean Up                                                         ║"
-        Write-Host "║                                                                        v0.1.0 ║"
+        Write-Host "║                                                                        v0.1.1 ║"
         Write-Host "╚═══════════════════════════════════════════════════════════════════════════════╝"
+}
+
+function Get-Report{
+    if($movereport -eq "1"){
+        Startbildschirm
+        Write-Host ""
+        Start-Transcript "$reportname" | Out-Null
+        Write-Host ""
+        Write-Host ""
+        Write-Host "  ${notice}: $movereportmessage1"
+        Write-Host ""
+    }
+    else{
+        Startbildschirm
+        Write-Host ""
+        Write-Host "  ${notice}: $movereportmessage2"
+        Write-Host ""
+    }
 }
 
 ### Temporärer Papierkorb anlegen ###
@@ -44,3 +69,7 @@ function Create-Junkfolder{
 
 $junkexists  = "Temporärer Papierkorb bereits angelegt."
 $junkcreated = "Temporärer Papierkorb angelegt."
+$notice        = "Hinweis"
+$reportname    = "$folder to $junkname $(Get-Date -Format yy.MM.dd-HHmmss).txt"
+$movereportmessage1 = "Die Aufzeichnung über verschobene Elemente wird durchgeführt."
+$movereportmessage2 = "Es wird keine Aufzeichnung über verschobene Elemente durchgeführt."
